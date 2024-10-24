cde8cd4c269045e8d377f7bd953aecf0e43ee344
Eugene Kuz <tir26@ukr.net>
2024-10-24 15:49:05 +0300

    add style to page not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+import css from "./NotFoundPage.module.css";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return <div className={css.notFoundWrapper}><span className={css.statusCode}>404</span> Page not found</div>;
 };
 
 export default NotFoundPage;
